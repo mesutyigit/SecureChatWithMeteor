e52feea1bc6ecd362a61160ac0b97d49549a490c
EbubekirOgden <ebubekirogden@gmail.com>
2015-07-07 13:16:28 +0300

    deneme

--- SecureChat/client/templates/profile/newPerson.js
+++ SecureChat/client/templates/profile/newPerson.js
@@ -1,14 +1,35 @@
+var name;
+var surname;
+var phoneNumnber;
+
 Template.newPerson.events({
     'keyup input.pp' :function(e, tmpl){
-        var name = tmpl.find('#name').value;
-        var surname = tmpl.find('#surname').value;
+        name = tmpl.find('#name').value;
+        surname = tmpl.find('#surname').value;
         name = name.toUpperCase();
         surname = surname.toUpperCase();
         $("#pic").html("");
-        if(name !== null)   $("#pic").append(name[0]);
+        if(!isEmpty(name))   $("#pic").append(name[0]);
         
-        if(surname !== null)    $("#pic").append(surname[0]);
+        if(!isEmpty(surname))    $("#pic").append(surname[0]);
+                          
+    },
+                          
+    'keyup #phoneNumber' : function(e, tmpl){
+        phoneNumber = tmpl.find('#phoneNumber').value;
+        if(!control())
+            document.getElementById("done").setAttribute("disabled");
+        else
+            document.getElementById("done").removeAttribute("disabled");
+                          
+                          
                           
     }
 })
 
+    control = function(){
+        if(!isEmpty(name) && !isEmpty(surname) && !isEmpty(phoneNumber)){
+            document.getElementById("done").removeAttribute("disabled");
+            return true;
+        }
+    }
