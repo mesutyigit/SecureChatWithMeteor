5b959d330ccf4c015e1ee3687581d88905b125b5
Ebubekir <ebubekirogden@gmail.com>
2015-10-25 02:32:50 +0300

    Update contacts.js

--- SecureChat/client/templates/contacts/contacts.js
+++ SecureChat/client/templates/contacts/contacts.js
@@ -18,40 +18,6 @@ if(Meteor.isCordova){
             
         }
     })
-     
-    owner = [];
-    prime = 13;
-    num = 6;
-    random = (Math.random() * 5) + 10 ;
-    random = Math.floor(random);
-    keyHelper = (Math.pow(num, random)) % prime;
-    Session.set('taked', false);
-
-    sendActKey = function(message, senderID){
-        keyGen.emit('keyGen', message, senderID);
-        alert("key tekrar yollandi");
-    };
-    
-    keyTaked.on(Meteor.userId(), function(message, ownerId){
-        if(!Session.get('taked')){
-            owner.push(ownerId);
-            sendActKey(keyHelper, owner);
-            var key = (Math.pow(message, random)) % prime;
-            Chats.insert({
-                owner : ownerId,
-                friend : Meteor.userId(),
-                key : key,
-                createdAt : new Date,
-                body : ""            
-            });
-            Chats.findOne({friend : Meteor.userId()}, {sort : {createdAt : -1}, limit : 1}).key;
-            Session.set('taked', true);
-        }
-        else{
-            alert("I don't want to live on this planet anymore");
-        }
-    });
-
     
 
 }
